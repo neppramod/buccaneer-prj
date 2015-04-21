bd1921bab94f410fc96873dbc774da3e76a05411
KamrulSumon <hasanm@goldmail.etsu.edu>
2015-04-21 16:28:20 -0400

    Expertise is added

---
+++ app/controllers/expertises_controller.rb
@@ -0,0 +1,59 @@
+class ExpertisesController < ApplicationController
+  def index
+    @expertises = Expertise.all
+  end
+
+  def show
+    @expertise = Expertise.find(params[:id])
+
+    @expertise.persons.each do |p|
+      puts "Show is showing a showey person => #{p.name}"
+    end
+
+    return @expertise
+  end
+
+  def new
+    @expertise = Expertise.new
+  end
+
+  def edit
+    @expertise = Expertise.find(params[:id])
+  end
+
+  def create
+   @expertise = Expertise.new(event_params)
+    
+   
+    if @expertise.save
+      redirect_to @expertise
+    else
+      render 'new'
+    end
+
+  end
+
+  def update
+    @expertise = Expertise.find(params[:id])
+
+   
+    if @expertise.update(event_params)
+      redirect_to @expertise
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @expertise = Expertise.find(params[:id])
+    @expertise.destroy
+
+    redirect_to expertises_path
+  end
+
+  private
+  def event_params
+    params.require(:expertise).permit(:expertise)
+  end
+
+end
---
+++ app/models/expertise.rb
@@ -0,0 +1,5 @@
+class Expertise
+  include Mongoid::Document
+  field :expertise, type: String
+  has_and_belongs_to_many :persons, autosave: true  
+end
