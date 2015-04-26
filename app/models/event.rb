e4b6b9a504c03f7f2a3be3ac16b674defb087f79
KamrulSumon <hasanm@goldmail.etsu.edu>
2015-04-26 16:48:11 -0400

    Search inital implementation

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,18 +4,23 @@ class Event
   field :eventName, type: String
      validates :eventName,  presence: true, length: { maximum: 50 } ,format: { with: /\A[a-zA-Z ]+\z/,
     message: "only allows letters and white space" }
+
   field :eventStartDate, type: String
      validates :eventStartDate,  presence: true, length: { maximum: 50 } ,format: { with: /\d{2}\/\d{2}\/\d{4}/,
     message: "must be date and time"}
+
   field :eventEndDate, type: String
      validates :eventEndDate,  presence: true, length: { maximum: 50 } ,format: { with: /\d{2}\/\d{2}\/\d{4}/,
     message: "must be date and time"} 
+
   field :location, type: String
      validates :location,  presence: true, length: { maximum: 50 } ,format: { with: /\A[a-zA-Z ]+\z/,
     message: "only allows letters and white space" }
+
   field :description, type: String
      validates :description,  presence: true, length: { maximum: 50 } ,format: { with: /\A[a-zA-Z ]+\z/,
     message: "only allows letters and white space" }
+    
   has_and_belongs_to_many :persons, autosave: true
 
   belongs_to :address
